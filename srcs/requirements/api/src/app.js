c1d67a871e478497bd16282bbbb2d48c25a2ae4b
maxdegers <maxdegers@gmail.com>
2025-09-02 12:27:47 +0200

    fix bug

--- srcs/requirements/api/src/app.js
+++ srcs/requirements/api/src/app.js
@@ -1,18 +1,28 @@
-'use strict'
-
-const path = require('node:path');
-const AutoLoad = require('@fastify/autoload');
-
+// Example of correct Fastify app structure
 module.exports = async function (fastify, opts) {
-    fastify.register(AutoLoad, {
-        dir: path.join(__dirname, 'plugins'),
-        options: Object.assign({}, opts)
-    });
+  // Your routes here
+}
 
-    fastify.register(AutoLoad, {
-        dir: path.join(__dirname, 'routes'),
-        options: Object.assign({}, opts)
-    });
-};
+// or if using options:
+const options = {}
+// Then use options variable
 
-module.exports.options = options
+
+// 'use strict'
+
+// const path = require('node:path');
+// const AutoLoad = require('@fastify/autoload');
+
+// module.exports = async function (fastify, opts) {
+//     fastify.register(AutoLoad, {
+//         dir: path.join(__dirname, 'plugins'),
+//         options: Object.assign({}, opts)
+//     });
+
+//     fastify.register(AutoLoad, {
+//         dir: path.join(__dirname, 'routes'),
+//         options: Object.assign({}, opts)
+//     });
+// };
+
+// module.exports.options = options
