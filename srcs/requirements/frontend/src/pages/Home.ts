e9f1530230fc3bc346496c6029be77aeced6abba
Guillaume Bertholio <guillaume.bertholio13@gmail.com>
2025-09-10 16:02:51 +0200

    add early user state

--- srcs/requirements/frontend/src/pages/Home.ts
+++ srcs/requirements/frontend/src/pages/Home.ts
@@ -3,19 +3,64 @@ import type { Page } from "../interface/gameInterface.js"
 export const Home: Page = {
   render() {
     return `
-      <div class="flex flex-col items-center justify-center h-full">
-        <h1 class="text-5xl font-bold text-indigo-600 mb-4">Bienvenue sur ft_transcendence</h1>
-        <p class="text-xl text-gray-700 mb-8 text-center max-w-md">
-          Découvrez le jeu, connectez-vous et amusez-vous !
-        </p>
-        <a href="#/game" class="px-8 py-3 bg-indigo-600 text-white rounded-lg text-lg font-semibold hover:bg-indigo-700 transition-colors">
-          Jouer
-        </a>  
-      </div>
+    <div class="flex flex-col items-center justify-center h-full">
+          <h1 class="text-5xl font-bold text-indigo-600 mb-4">Bienvenue sur ft_transcendence</h1>
+          <p class="text-xl text-gray-700 mb-8 text-center max-w-md">
+            Découvrez le jeu, connectez-vous et amusez-vous !
+          </p>
+<button command="show-modal" commandfor="dialog" class="px-8 py-3 bg-indigo-600 text-white rounded-lg text-lg font-semibold hover:bg-indigo-700 transition-colors">Sign in</button>
+<el-dialog>
+  <dialog id="dialog" aria-labelledby="dialog-title" class="fixed inset-0 size-auto max-h-none max-w-none overflow-y-auto bg-transparent backdrop:bg-transparent">
+      <el-dialog-backdrop class="fixed inset-0 bg-gray-900/50 transition-opacity data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in"></el-dialog-backdrop>
+
+    <div tabindex="0" class="flex min-h-full items-end justify-center p-4 text-center focus:outline-none sm:items-center sm:p-0">
+      <el-dialog-panel class="relative transform overflow-hidden rounded-lg bg-gray-800 text-left shadow-xl outline -outline-offset-1 outline-white/10 transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in sm:my-8 sm:w-full sm:max-w-lg data-closed:sm:translate-y-0 data-closed:sm:scale-95">
+        <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+          <div class="sm:flex sm:items-start">
+            <div class="mx-auto flex size-12 shrink-0 items-center justify-center rounded-full bg-red-500/10 sm:mx-0 sm:size-10">
+              <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" data-slot="icon" aria-hidden="true" class="size-6 text-red-400">
+                <path d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z" stroke-linecap="round" stroke-linejoin="round" />
+              </svg>
+            </div>
+        <div class="bg-gray-700/25 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+        <form id="userForm" class="max-w-sm mx-auto">
+          <div class="mb-5">
+            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your username</label>
+            <input type="username" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="username" required />
+          </div>
+          <div class="mb-5">
+            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
+            <input type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 " placeholder="password" required />
+          </div>
+          <div class="flex items-start mb-5">
+            <div class="flex items-center h-5">
+              <input id="remember" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded-sm bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"               />
+            </div>
+            <label for="remember" class="ms-2 px-3 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
+          </div>
+            <div class="flex justify-end space-x-4">
+            <button command="close" commandfor="dialog" onclick="history.go(0)" class="rounded-md bg-white px-5 py-2.5 text-sm font-semibold text-gray-700 ring-1 ring-inset ring-gray-300 hover:bg-gray-100 dark:bg-gray-700 dark:text-white dark:ring-white">
+              Cancel
+            </button>
+            <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+              Sign in
+            </button>
+          </div>
+          </form>
+        </div>
+      </el-dialog-panel>
+    </div>
+  </dialog>
+</el-dialog>
+    </div>
     `;
   },
 
+
   mount(root) {
     // Add any Home-specific event listeners here
+    document.getElementById('userForm').addEventListener('submit', async function (e) {
+      e.preventDefault();
+    });
   }
 }
