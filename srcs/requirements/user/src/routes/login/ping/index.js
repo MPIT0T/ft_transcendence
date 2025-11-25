790a51ab7912870d71b67ebdd1fb8040da82b45d
Guillaume Bertholio <gberthol@z3r3p5.42lyon.fr>
2025-11-25 15:13:59 +0100

    add a lot of feature including graph and change name issue

--- srcs/requirements/user/src/routes/login/ping/index.js
+++ srcs/requirements/user/src/routes/login/ping/index.js
@@ -1,14 +1,19 @@
 'use strict'
 const db = require("../../../db.js");
 const {sqliteCurrentTimestamp} = require("../../../utils.js");
+const {errorToken} = require("../../../utils");
 
 async function loginPingRoute(fastify, options) {
     fastify.post('/', async (req, reply) => {
         const updateStmt = db.prepare('UPDATE users SET last_ping = ? WHERE username = ?');
-        const result = await updateStmt.run(sqliteCurrentTimestamp(), req.body.username);
+        const {username} = req.body || {};
+
+        if (errorToken(req.headers['authorization'], username))
+            return reply.status(401).send({error: "invalid token"});
+        const result = await updateStmt.run(sqliteCurrentTimestamp(), username);
         if (result.changes === 0)
             return reply.status(404).send({error: 'Utilisateur non trouvé'});
-        return reply.status(200).send({message:'Ok'});
+        return reply.status(200).send({message: 'Ok'});
     });
 }
 
